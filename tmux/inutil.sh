2d39dee2faab6d69db07ff51cbc6ffca3a160a65
marc fuentes <marc.fuentes@inria.fr>
2016-02-10 17:58:43 +0100

    MDB tmux : complexifier le script d'exemple

--- tmux/inutil.sh
+++ tmux/inutil.sh
@@ -4,12 +4,26 @@ function get_current_pane()
  get_current_pane=`tmux list-panes | grep active | cut -d ':' -f 1`
 }
 
+function number_of_panes()
+{
+  number_of_panes=`tmux list-panes | wc -l` 
+}
+
 get_current_pane
-for i in $(seq 1 10); do
+for i in $(seq 1 4); do
         if  (( $i % 2 == 0))
     then
         tmux split-window -h -t $get_current_pane;
     else 
         tmux split-window -v -t $get_current_pane
 fi
+done
+number_of_panes
+for i in $(seq 1 $number_of_panes); do
+        tmux send-keys -t:.$i 'echo COUCOU' C-m
+done
+tours=$((30 * $number_of_panes))
+for i in $(seq 1 $tours); do
+        p=$(($i % $number_of_panes)) 
+        tmux select-pane -t:.$p 
 done
