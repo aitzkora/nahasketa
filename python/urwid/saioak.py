29675e6e819338234361a80e8136ea40b9bd7cb7
marc fuentes <marc.fuentes@inria.fr>
2016-12-21 09:35:02 +0100

    urwid : EneBotoia kadroa dauka orain

--- python/urwid/saioak.py
+++ python/urwid/saioak.py
@@ -3,10 +3,11 @@ import urwid
 
 class EneBotoia(urwid.Text):
     def __init__(self, label):
-        w = self.__super.__init__(label)
-        w = urwid.LineBox(w)
+        self.__super.__init__(label)
+        w = urwid.LineBox(self)
         w = urwid.Padding(w, 'center', len(label) + 2)
-        w = urwid.Filler(w, 'middle', height=1)
+        #w = urwid.Filler(w, 'middle', height=1)
+        self.view = w
 
 
 def exit_on_q(key):
@@ -16,7 +17,7 @@ def exit_on_q(key):
 
 button1 = EneBotoia(u'Berriz')
 button2 = EneBotoia(u'Erraza')
-empile = urwid.Columns([button1, button2])
-top = urwid.Filler(empile, valign='top')
+empile = urwid.Columns([button1.view, button2.view])
+top = urwid.Filler(empile, valign='middle')
 loop = urwid.MainLoop(top, unhandled_input=exit_on_q)
 loop.run()
