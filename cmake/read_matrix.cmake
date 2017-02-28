3bd6f32a97375c4b16aecbdf904ce67882c13ccd
aitzkora <marc.fuentes@tiscali.it>
2017-02-28 11:34:22 +0100

    cmake : read_matrix and display_matrix seem to work, add an example file

--- cmake/read_matrix.cmake
+++ cmake/read_matrix.cmake
@@ -1,14 +1,16 @@
 # Read a matrix in a text file, where each entry in separated of the next
 # by some whitespaces
-macro(read_matrix fileName outputMatrix outputnbCols)
+# @fileName : filename containing the matrix
+# @matrix : name of the variable where we store the read matrix
+# @nbColsOut : name of the variable containing the number of columns
+macro(read_matrix fileName matrix nbColsOut)
     file(STRINGS ${fileName} fileData)
-    message(STATUS "Reading the matrix in the text file ${fileName}")
     list(LENGTH fileData nbRows)
-    message(STATUS "nbRows = ${nbRows}")
     list(GET fileData 0 rowZero)
     string(REGEX MATCHALL "[^\ ]+\ +|[^\ ]+$" rowZeroList "${rowZero}")
     list(LENGTH rowZeroList nbCols)
-    message(STATUS "nbCols = ${nbCols}")
+    set(nbColsOut ${nbCols})
+    message(STATUS "Reading the matrix in the text file ${fileName} (${nbRows}, ${nbCols})")
     math(EXPR nbRowsMinusOne "${nbRows} - 1")
     math(EXPR nbColsMinusOne "${nbCols} - 1")
     foreach(rowNumber RANGE ${nbRowsMinusOne})
@@ -17,16 +19,28 @@ macro(read_matrix fileName outputMatrix outputnbCols)
         foreach(colNumber RANGE ${nbColsMinusOne})
             list(GET rowDataList ${colNumber} entryIJ)
             string(REPLACE " " "" entryIJWithoutSpaces ${entryIJ})
-            message(STATUS "(${rowNumber},${colNumber}) = ${entryIJWithoutSpaces}")
+            list(APPEND ${matrix} ${entryIJWithoutSpaces})
         endforeach()
     endforeach()
 endmacro(read_matrix)
 
-macro(display_matrix matrix)
-    list(LENGTH matrix nbRows)
-    list(GET matrix 0 rowZero)
+# to display matrix
+# @matrix : flat list with all elements of the matrix
+# @nbCols : number of columns for the matrix
+macro(display_matrix matrix nbCols)
+    list(LENGTH ${matrix} nbElements)
+    math(EXPR nbElementsMinusOne "${nbElements} - 1")
+    foreach(i RANGE ${nbElementsMinusOne})
+        math(EXPR isBeginningOfRow "(${i}+1) % ${nbCols}")
+                list(GET ${matrix} ${i} elem)
+        set(row "${row} ${elem}")
+        if(${isBeginningOfRow} EQUAL "0")
+            message(STATUS ${row})
+            set(row "")
+        endif()
+endforeach()
 endmacro(display_matrix)
 
-
-
-read_matrix("table_test")
+# a test
+read_matrix("mat.txt" mat nbCols)
+display_matrix(mat ${nbCols})
