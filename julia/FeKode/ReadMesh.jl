d416a8aace1234008ac7c22542387f1035f1192e
aitzkora <marc.fuentes@tiscali.it>
2017-09-06 16:16:29 +0200

    add commentaries to ReadMesh

--- julia/FeKode/ReadMesh.jl
+++ julia/FeKode/ReadMesh.jl
@@ -1,13 +1,14 @@
 type Mesh
     points::Array{Float64,2}
-    cells::Array{Int32,2}
+    cells::Array{Array{Int32}}
 end
+
 using PyCall
-unshift!(PyVector(pyimport("sys")["path"]),"/home/fux/configs/VTK8/lib/python2.7/site-packages/")
 @pyimport vtk
 @pyimport vtk.util as vu
 @pyimport vtk.util.numpy_support as vuns
 function readMeshFromFile(fileName::String)
+    i::Int32
     reader=vtk.vtkUnstructuredGridReader()
     reader["SetFileName"](fileName)
     reader["Update"]()
@@ -15,19 +16,20 @@ function readMeshFromFile(fileName::String)
     pts = out[:GetPoints]()
     pointsData = pts[:GetData]()
     points = vuns.vtk_to_numpy(pointsData)
+    nbPoints = size(points, 1)
     clls = out[:GetCells]()
     cllData = clls[:GetData]()
     cellsRaw = vuns.vtk_to_numpy(cllData)
+    # Re-organize Cell raw data into an array of array
     i = 1
     nbCells = out[:GetNumberOfCells]()
     cellIdx =1
     cells = fill(Int32[],nbCells)
-    while i < size(cellsRaw)[1]
+    while i < size(cellsRaw,1)
          currentCellSize = cellsRaw[i]
-         println("domain = $(i+1)-$(i+currentCellSize)")
          cells[cellIdx] = cellsRaw[i+1:i+currentCellSize]
          i += currentCellSize+1
          cellIdx += 1
     end
-    return points,cells
+    return Mesh(points,cells)
 end
