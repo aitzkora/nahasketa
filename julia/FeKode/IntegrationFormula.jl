fb082aa624782044014620a4fc5abfb561b1f6d8
aitzkora <marc.fuentes@tiscali.it>
2017-09-28 12:03:29 +0200

    Suppress the using of Match.jl for integration formulae using approx operator for testing

--- julia/FeKode/IntegrationFormula.jl
+++ julia/FeKode/IntegrationFormula.jl
@@ -12,39 +12,44 @@ type IntegrationFormula
    weights::Array{Float64}
 end
 
-
-using Match
 function IntegrationFormula(dim::Int64, order::Int64)
-     @match (dim, order) begin
-         (0,_) => IntegrationFormula(0, [[0]], fill(1.,1))
-         (1, 2||3) => IntegrationFormula(1, [[(-sqrt(1/3.) + 1.0) / 2 (sqrt(1/3.) + 1.0) / 2]], [0.5, 0.5])
-         (2,2) => IntegrationFormula(2, [[2/3. 1/6.]; [1/6. 1/6.]; [1/6. 2/3.]]', fill(1/6., 3))
-         (2,3) => begin inverse_tan = atan(0.75);
-                     cos_third = cos(inverse_tan/3.0);
-                     sin_third = sin(inverse_tan/3.0);
-                     a = sin_third/(2*sqrt(3.0)) - cos_third/6.0 + 1.0/3.0;
-                     b = - sin_third/(2*sqrt(3.0)) - cos_third/6.0 + 1.0/3.0;
-                     c = cos_third/3.0 + 1.0/3.0;
-                     IntegrationFormula(2, [[a b]; [a c]; [b a]; [b c]; [c a]; [c b] ]', fill(1/12., 6))
-                  end
-         (3, 2) => begin sqrtFifth = 1.0 / sqrt(5.0);
-                      a = (1.0 + 3.0 * sqrtFifth) / 4.0;
-                      b = (1.0 - sqrtFifth) / 4.0;
-                      IntegrationFormula(3,[[a b b]; [b a b]; [b b a]; [b b b]]' , fill(1/24., 4))
-                  end
-         (3, 3) => begin rootSeventeen = sqrt(17.0);
-                      rootTerm = sqrt(1022.0 - 134.0 * rootSeventeen);
-                      b = (55.0 - 3.0*rootSeventeen + rootTerm) / 196;
-                      d = (55.0 - 3.0*rootSeventeen - rootTerm) / 196;
-                      a = 1.0 - 3.0 * b;
-                      c = 1.0 - 3.0 * d;
-                      w1 = (20.0*d*d - 10.0*d + 1.0)/(240.0*(2.0*d*d - d - 2.0*b*b + b));
-                      w2 = 1.0/24.0 - w1;
-                      IntegrationFormula(3,
-                                         [[a b b]; [b a b]; [b b a]; [b b b]; [c d d]; [d c d]; [d d c]; [d d d]]',
-                                         [fill(w1, 4); fill(w2, 4)])
-                   end
-        (_, _) => IntegrationFormula(-1, Float64[], Float64[])
+     if (dim == 0)
+            return IntegrationFormula(0, [[0];]', [1.])
+     elseif dim == 1 && order in 2:3
+         return IntegrationFormula(1, [[(-sqrt(1/3.) + 1.0) / 2 (sqrt(1/3.) + 1.0) / 2];], [0.5, 0.5])
+     elseif (dim,order) == (2,2)
+         return IntegrationFormula(2, [[2/3. 1/6.]; [1/6. 1/6.]; [1/6. 2/3.]]', fill(1/6., 3))
+     elseif (dim,order) == (2,3)
+          begin inverse_tan = atan(0.75);
+                cos_third = cos(inverse_tan/3.0);
+                sin_third = sin(inverse_tan/3.0);
+                a = sin_third/(2*sqrt(3.0)) - cos_third/6.0 + 1.0/3.0;
+                b = - sin_third/(2*sqrt(3.0)) - cos_third/6.0 + 1.0/3.0;
+                c = cos_third/3.0 + 1.0/3.0;
+                return  IntegrationFormula(2, [[a b]; [a c]; [b a]; [b c]; [c a]; [c b] ]', fill(1/12., 6))
+          end
+     elseif (dim, order) == (3, 2)
+         begin
+             sqrtFifth = 1.0 / sqrt(5.0);
+             a = (1.0 + 3.0 * sqrtFifth) / 4.0;
+             b = (1.0 - sqrtFifth) / 4.0;
+             return IntegrationFormula(3,[[a b b]; [b a b]; [b b a]; [b b b]]' , fill(1/24., 4))
+         end
+     elseif (dim, order) == (3, 3)
+         begin rootSeventeen = sqrt(17.0);
+             rootTerm = sqrt(1022.0 - 134.0 * rootSeventeen);
+             b = (55.0 - 3.0*rootSeventeen + rootTerm) / 196;
+             d = (55.0 - 3.0*rootSeventeen - rootTerm) / 196;
+             a = 1.0 - 3.0 * b;
+             c = 1.0 - 3.0 * d;
+             w1 = (20.0*d*d - 10.0*d + 1.0)/(240.0*(2.0*d*d - d - 2.0*b*b + b));
+             w2 = 1.0/24.0 - w1;
+             return IntegrationFormula(3,
+             [[a b b]; [b a b]; [b b a]; [b b b]; [c d d]; [d c d]; [d d c]; [d d d]]',
+             [fill(w1, 4); fill(w2, 4)])
+         end
+     else
+         error("IntegrationFormula: no existing integration formula for these arguments")
      end
 end
 
@@ -54,11 +59,12 @@ integrate function f on the unit simplex using gauß formula
 """
 function integrate(f, form::IntegrationFormula)
     @assert size(form.points, 2) ==  size(form.weights,1) "integrate :  points and weights do no have compatible dimensions"
-    return mapslices(f, form.points, 1)*form.weights
+    return (mapslices(f, form.points, 1)*form.weights)[1]
 end
 
 using Base.Test
-@test_approx_eq_eps integrate(prod,IntegrationFormula(2,2)) 1./24 1e-12 # integrate(x*y,(y,0,1-x),(x,0,1))
-@test_approx_eq_eps integrate(x->x[1]^2*x[2], IntegrationFormula(2,3)) 1/60. 1e-12 #integrate(x*x*y,(y,0,1-x),(x,0,1))
-@test_approx_eq_eps integrate(x->x[1]*x[2],IntegrationFormula(3,2)) 1/120. 1e-12 #integrate(x*y,(z,0,1-y-x),(y,0,1-x),(x,0,1))
-@test_approx_eq_eps integrate(prod,IntegrationFormula(3,3)) 1/720. 1e-12 # integrate(x*y*z,(z,0,1-y-x),(y,0,1-x),(x,0,1))
+@test integrate(x->x[1]*x[1], IntegrationFormula(1,2)) ≈ 1./3 atol = 1e-12
+@test integrate(prod,IntegrationFormula(2,2)) ≈ 1 ./ 24 atol=1.0e-12 # integrate(x*y,(y,0,1-x),(x,0,1))
+@test integrate(x->x[1]^2*x[2], IntegrationFormula(2,3)) ≈ 1/60. atol= 1e-12 #integrate(x*x*y,(y,0,1-x),(x,0,1))
+@test integrate(x->x[1]*x[2],IntegrationFormula(3,2)) ≈1/120. atol=1e-12 #integrate(x*y,(z,0,1-y-x),(y,0,1-x),(x,0,1))
+@test integrate(prod,IntegrationFormula(3,3)) ≈ 1/720. atol= 1e-12 # integrate(x*y*z,(z,0,1-y-x),(y,0,1-x),(x,0,1))
