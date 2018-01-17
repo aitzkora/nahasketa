cb3ef3b40df3d47cff0878e5c0e89cb34d804a49
aitzkora <marc.fuentes@tiscali.it>
2018-01-17 17:39:45 +0100

    continue on ReadMeshAscii

--- julia/FeKode/ReadMeshAscii.jl
+++ julia/FeKode/ReadMeshAscii.jl
@@ -1,8 +1,17 @@
 include("Mesh.jl")
 function readMeshFromFileAsciiVtk(fileName::String)
+    m=Mesh(zeros(0,0),Array{Int64}[])
     fp=open(fileName)
     lines=readlines(fp)
     close(fp)
-    i = find(lines .== "ASCII") # we ignore everything before ASCII
+    if !(lines[3] == "ASCII")
+        println("we read only ASCII files")
+        return m
+    end
+    if !(lines[4] == "DATASET UNSTRUCTURED_GRID")
+        println("we read only unstructured grid")
+        return m
+    end
+    ptsStr = match(r"\d+",lines[5])
     return lines
 end
