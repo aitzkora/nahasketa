950d5eae2ad7da6e91fc86673ea875acfd0e2aa9
aitzkora <marc.fuentes@tiscali.it>
2018-02-28 12:05:41 +0100

    computation of mass matrix

--- julia/FeKode/squareGenerate.jl
+++ julia/FeKode/squareGenerate.jl
@@ -5,11 +5,11 @@ beware that cells indexing begins to 1 (for julia, not zero (vtk))
 """
 
 function squareMeshGenerate(xMin::Float64, xMax::Float64, yMin::Float64, yMax::Float64, nX::Int, nY::Int)
-x=linspace(xMin ,xMax ,nX)
-y=linspace(yMin ,yMax ,nY)
-points = hcat([[i,j,0.] for i in x for j in y]...)'
-cells = vcat([vcat([(i-1)*nX+j (i-1)*nX+j+1 i*nX+j+1],[(i-1)*nX+j i*nX+j+1 i*nX+j]) for i=1:nX-1 for j=1:nY-1]...)
-return Mesh(points ,[cells[i,:] for i=1:size(cells,1)])
+   x=linspace(xMin, xMax, nX)
+   y=linspace(yMin, yMax, nY)
+   points = hcat([[i,j,0.] for i in x for j in y]...)'
+   cells = vcat([vcat([(i-1)*nX+j (i-1)*nX+j+1 i*nX+j+1],[(i-1)*nX+j i*nX+j+1 i*nX+j]) for i=1:nX-1 for j=1:nY-1]...)
+   return Mesh(points ,[cells[i,:] for i=1:size(cells,1)])
 end
 
 using Base.Test
