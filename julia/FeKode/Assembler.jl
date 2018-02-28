9939e6c37f3f0191f37e70bcce8c9600e4f3a200
marc fuentes <marc.fuentes@inria.fr>
2018-02-28 19:38:49 +0000

    tests now passes in Assembler.jl for jacobian

--- julia/FeKode/Assembler.jl
+++ julia/FeKode/Assembler.jl
@@ -1,27 +1,25 @@
 include("IntegrationFormula.jl")
-uses Base.test
-
 """
-create the $B_k$ matrix which corresponds to the linear map  from the 
-reference element $ T = \{ (x,y,z) : x,y,z ≥ 0 x+y+z = 1 \} $ to the
+create the B_k matrix which corresponds to the linear map  from the 
+reference element T = \{ (x,y,z) : x,y,z ≥ 0 x+y+z = 1 \} to the
 current triangle 
 Input :
 ------
 x : a 3 x m matrix containing all the points of the current element
 Output :
 -------
-$B_k = [ p_m - p_1, \cdot, p_2 ... p_1]
+B_k = [ p_m - p_1, \cdot, p_2 ... p_1]
 """
 function mapRefToLocal(x::Array{Float64,2})
-@assert(size(x, 1) == 3)
-return x[:,2:end].-x[:,1:1] # or cumsum(diff(x,2),2)
+    @assert(size(x, 1) == 3)
+    return x[:,2:end].-x[:,1:1] # or cumsum(diff(x,2),2)
 end
 
-a = reshape([ gcd(i,j) for i=1:4 for j=1:3], 3,4)
-@test mapRefToLocal(a[:,1:2]) == ones(3)
-@test mapRefToLocal(a[:,1:3]) == [1 2; 1 2; 1 2]
-@test mapRefToLocal(a[:,1:4]) == kron(ones(3,1),range(1,3)')
-
+using Base.Test
+a = reshape([ gcd(i,j) + 0. for i=1:4 for j=1:3], 3,4)
+@test mapRefToLocal(a[:,1:2]) ≈ [0, 1, 0] atol=1e-12
+@test mapRefToLocal(a[:,1:3]) == [0 0; 1 0; 0 2]
+@test mapRefToLocal(a[:,1:4]) == [0 0 0; 1 0 1; 0 2 0]
 
 """ 
 jacobian(x)
@@ -31,25 +29,25 @@ compute the jacobian of the current element
 """
 function jacobian(x::Array{Float64,2})
     m = mapRefToLocal(x)
-    n = size(x, 1)
+    n = size(m, 2)
     @assert (n in 1:3)
     if (n == 1)
         return norm(m)
-    else if (n == 2)
+    elseif (n == 2)
         return norm(cross(m[:,1], m[:, 2]))
-    else if (n == 3)
+    elseif (n == 3)
         return det(m)
     end
 end
 
-a = reshape([ gcd(i,j) for i=1:4 for j=1:3], 3,4)
-@test jacobian(a[:,1:2]) == 3
-@test jacobian(a[:,1:3]) == [1 2; 1 2; 1 2]
-@test jacobian(a[:,1:4]) == kron(ones(3,1),range(1,3)')
+a = reshape([ gcd(i,j) + 0. for i=1:4 for j=1:3], 3,4)
+@test jacobian(a[:,1:2]) == 1
+@test jacobian(a[:,1:3]) == 2
+@test jacobian(a[:,1:4]) == 0
 
 
-
-function stiffnesMatrix(mesh)
+include("Mesh.jl")
+function stiffnesMatrix(mesh::Mesh)
     m = size(mesh.cells, 1)
     n = size(mesh.points, 1)
     A = zeros(n, n)
