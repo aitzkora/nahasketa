5a5ee599e768bb04d894dff56159f98e5fca8792
aitzkora <marc.fuentes@tiscali.it>
2018-03-06 18:04:29 +0100

    Now computeφAndDφOnGaußPoints now works for P1Basis (tested for dim=2)

--- julia/FeKode/Assembler.jl
+++ julia/FeKode/Assembler.jl
@@ -48,12 +48,22 @@ a = reshape([ gcd(i,j) + 0. for i=1:4 for j=1:3], 3,4)
 
 include("BasisFunction.jl")
 function computeφAndDφOnGaußPoints(fun::BasisFunction, form::IntegrationFormula)
-m = size(fun.φ, 1)
-n = size(form.points, 1)
-M = Float64[fun.φ[i](form.points[k]) for i=1:m, k=1:n]
-N = Float64[fun.Dφ[i,j](form.points[k]) for i=1:m, j=1:m , k=1:n]
+m, n = size(fun.Dφ)
+p = size(form.points, 2) # beware that points are stored vertically
+M = Float64[fun.φ[i](form.points[:, k]) for i=1:m, k=1:p]
+N = Float64[fun.Dφ[i,j](form.points[:, k]) for i=1:m, j=1:n, k=1:p]
 return M,N
 end
+
+## FIXME : add test for others cases than 2D dimension
+m, n = computeφAndDφOnGaußPoints(P1Basis(2), IntegrationFormula(2, 2))
+m_ref = [1 4 1; 4 1 1 ; 1 1 4] / 6.
+n_ref = [-1. -1; 1 0; 0 1]
+n_ref = repeat(reshape(n_ref,3,2,1), outer=(1,1,3))
+@test norm(m - m_ref) ≈ 0. atol = 1.e-12
+@test norm(vec(n - n_ref)) ≈ 0. atol = 1.e-12
+
+
 
 
 """
@@ -90,4 +100,5 @@ function stiffnesAndMassMatrix(mesh::Mesh, order::Int64, fun::BasisFunction)
         mElem = φ * Diagonal(jk.*form.weights)*φ'
         M += sparse(cartesianProduct(indices, indices)...,mElem[:], n,n)
     end
+    return K,M
 end
