dded6bef157031fb178836b29e0a9b4742085d12
aitzkora <marc.fuentes@tiscali.it>
2018-05-16 17:24:22 +0200

    Change diagonal coefficients of points on the boundary to ones

--- julia/FeKode.jl/zut.jl
+++ julia/FeKode.jl/zut.jl
@@ -1,7 +1,7 @@
 using FeKode
 m = FeKode.squareMeshGenerate(0.,1., 0. , 1., 3, 3)
 fe = FeKode.P1Basis(2)
-M,K = FeKode.stiffnesAndMassMatrix(m, 2, 2, fe)
+M, K = FeKode.stiffnesAndMassMatrix(m, 2, 2, fe)
 u = (x,y,z) -> y.*(1-y).*x.^3
 f = (x,y,z) -> 6*x.*y.*(1-y)-2*x.^3
 F = f(m.points[:,1], m.points[:,2], m.points[:,3])
@@ -12,8 +12,7 @@ sort!(boundary)
 
 # substract boundary contribution to the right hand side
 F -= K[:, boundary] * u(m.points[boundary,1], m.points[boundary,2], m.points[boundary,3])
-indexes=filter(x->isempty(searchsorted(boundary,x)),1:size(F,1))
-Fn = sparsevec((1:size(F,1))[indexes], F[indexes])
+F[boundary] = u(m.points[boundary,1], m.points[boundary,2], m.points[boundary,3])
 
 # suppress rows and columns from the K matrix
 I,J,V=findnz(K)
@@ -23,12 +22,16 @@ Vn = Float64[]
 
 for i = 1:size(I, 1)
     IIsNotBoundary = isempty(searchsorted(boundary, I[i]))
-    JIsNotBoundary = isempty(searchsorted(boundary,J[i]))
+    JIsNotBoundary = isempty(searchsorted(boundary, J[i]))
     if IIsNotBoundary && JIsNotBoundary
         append!(In, I[i])
         append!(Jn, J[i])
         append!(Vn, V[i])
     end
 end
+append!(In, boundary)
+append!(Jn, boundary)
+append!(Vn, ones(size(boundary)))
+
 Kn= sparse(In,Jn,Vn)
-un = Kn\Fn
+sol=Kn\F
