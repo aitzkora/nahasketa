22821f67d40248b5aa2c442f09c126a4da5c272b
marc fuentes <marc.fuentes@inria.fr>
2018-05-21 18:46:36 +0200

    start to 1D mesh generating functions

--- julia/FeKode.jl/src/squareGenerate.jl
+++ julia/FeKode.jl/src/squareGenerate.jl
@@ -2,13 +2,26 @@
 generate a plane mesh for the square [xMin, xMax] x [ yMin, yMax] with nX x nY points
 beware that cells indexing begins to 1 (for julia, not zero (vtk))
 """
-function squareMeshGenerate(xMin::Float64, xMax::Float64, yMin::Float64, yMax::Float64, nX::Int, nY::Int)
+function meshGenerate2D(xMin::Float64, xMax::Float64, yMin::Float64, yMax::Float64, nX::Int, nY::Int)
    x=linspace(xMin, xMax, nX)
    y=linspace(yMin, yMax, nY)
    points = hcat([[i,j,0.] for i in x for j in y]...)'
    cells = vcat([vcat([(i-1)*nX+j (i-1)*nX+j+1 i*nX+j+1],[(i-1)*nX+j i*nX+j+1 i*nX+j]) for i=1:nX-1 for j=1:nY-1]...)
    ε=1e-20
    isBoundary = (x,y,z) -> abs(x-xMin)<ε || abs(x-xMax) <ε || abs(y-yMin) < ε || abs(y-yMax) < ε
+   boundary=filter(x->isBoundary(points[x,:]...), 1:size(points,1))
+   return Mesh(points ,[cells[i,:] for i=1:size(cells,1)], boundary)
+end
+"""
+generate a line mesh for the segment [xMin, xMax] with nX points
+beware that cells indexing begins to 1 (for julia, not zero (vtk))
+"""
+function meshGenerate1D(xMin::Float64, xMax::Float64, nX::Int)
+   x=linspace(xMin, xMax, nX)
+   points = hcat([[i, 0, 0. ] for i in x]...)'
+   cells = vcat([vcat([(i-1) i]) for i=1:nX-1]...)
+   ε=1e-20
+   isBoundary = (x,y,z) -> abs(x-xMin)<ε || abs(x-xMax) <ε
    boundary=filter(x->isBoundary(points[x,:]...), 1:size(points,1))
    return Mesh(points ,[cells[i,:] for i=1:size(cells,1)], boundary)
 end
--- julia/FeKode.jl/test/Meshes.jl
+++ julia/FeKode.jl/test/Meshes.jl
@@ -1,7 +1,13 @@
 @testset "Mesh Generation" begin
 # test on a very simple square
-mk1 = FeKode.squareMeshGenerate(0., 1., 0., 1., 2, 2)
+m1 = FeKode.meshGenerate2D(0., 1., 0., 1., 2, 2)
 @test m1.cells == [[1, 2, 4], [1, 4, 3]]
 @test m1.points == [[0. 0. 0.]; [0. 1. 0.];  [1. 0. 0.]; [1. 1. 0.];]
 @test m1.isOnBoundary == 1:4
+
+# test on a very simple line 
+m2 = FeKode.meshGenerate1D(0., 1., 2)
+@test m2.cells == [[1, 2], [2, 3]]
+@test m2.points == [[0. 0. 0.]; [0.5 0. 0.];  [1. 0. 0.]; ]
+@test m2.isOnBoundary == [ 1, 3];
 end
