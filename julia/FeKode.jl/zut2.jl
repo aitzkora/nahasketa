9dbc7b4fd35bfc2b643c51c13e34be6c47338268
marc fuentes <marc.fuentes@inria.fr>
2018-05-31 23:28:33 +0200

    now the 1D laplacian problem works

--- julia/FeKode.jl/zut2.jl
+++ julia/FeKode.jl/zut2.jl
@@ -3,8 +3,9 @@ N= 10
 m = FeKode.meshGenerate1D(0.,1., N)
 fe = FeKode.P1Basis(1)
 K, M = FeKode.stiffnesAndMassMatrix(m, 1, 2, fe)
+X = m.points[:,1]
 u = (x,y,z) -> x.^3
-f = (x,y,z) -> 6*x
+f = (x,y,z) -> -6*x
 F = M * f(m.points[:,1], m.points[:,2], m.points[:,3])
 
 #determine boundary
@@ -16,18 +17,18 @@ F -= K[:, boundary] * u(m.points[boundary,1], m.points[boundary,2], m.points[bou
 F[boundary] = u(m.points[boundary,1], m.points[boundary,2], m.points[boundary,3])
 
 # suppress rows and columns from the K matrix
-I,J,V=findnz(K)
+Ii,Ji,Vi=findnz(K)
 In = Int64[]
 Jn = Int64[]
 Vn = Float64[]
 
-for i = 1:size(I, 1)
-    IIsNotBoundary = isempty(searchsorted(boundary, I[i]))
-    JIsNotBoundary = isempty(searchsorted(boundary, J[i]))
+for i = 1:size(Ii, 1)
+    IIsNotBoundary = isempty(searchsorted(boundary, Ii[i]))
+    JIsNotBoundary = isempty(searchsorted(boundary, Ji[i]))
     if IIsNotBoundary && JIsNotBoundary
-        append!(In, I[i])
-        append!(Jn, J[i])
-        append!(Vn, V[i])
+        append!(In, Ii[i])
+        append!(Jn, Ji[i])
+        append!(Vn, Vi[i])
     end
 end
 append!(In, boundary)
@@ -35,4 +36,5 @@ append!(Jn, boundary)
 append!(Vn, ones(size(boundary)))
 
 Kn= sparse(In,Jn,Vn)
-sol=Kn\F;
+sol=Kn\F
+norm(sol-u(m.points[:,1],m.points[:,2], m.points[:,3]))
