f622b087a43d4e6a970d0878bdc1121c56e8da84
aitzkora <marc.fuentes@tiscali.it>
2019-01-05 00:57:46 +0100

    the grep basic example in fortran seems to work

--- fortran/grep_exo.f90
+++ fortran/grep_exo.f90
@@ -1,7 +1,8 @@
 program grep
   implicit none
-  integer :: i, narg
+  integer :: i, narg, unit_read=65, ios
   character(len=80):: param, command, pattern, filename
+  character(len=200) :: current_line
 
   narg = command_argument_count()
   if (narg < 2) then
@@ -10,7 +11,17 @@ program grep
       stop
   end if
   call get_command_argument( 1, pattern)
-
   call get_command_argument( 2, filename)
+
+  open(unit=unit_read, file= filename, iostat=ios)
+  if ( ios /= 0 ) stop "cannot open "// trim(filename)
+  i = 1
+  do
+      read(unit_read, '(A)', iostat=ios) current_line
+      if (index(current_line, trim(pattern)) /= 0) print '(i0A)', i, ":"//current_line
+      if (ios /= 0) exit
+      i = i + 1
+  end do
+  close( unit_read )
 
 end program grep
