c61789a01d9a053abca7e176a8c096aa84fc3e97
aitzkora <marc.fuentes@tiscali.it>
2019-01-22 15:08:58 +0100

    Julia bindings : modify test_myalloc using Libdl

--- julia/interfaces/C/test_myalloc.jl
+++ julia/interfaces/C/test_myalloc.jl
@@ -1,21 +1,26 @@
+using Libdl: dlopen, dlsym
+lib_alloc = dlopen("libmyalloc")
+all_ptr, fre_ptr, set_ptr, get_ptr = map(x->dlsym(lib_alloc, x), [:my_alloc, :my_free, :set_values, :get_values])
+
+
 function alloc(n::Int)
-    ret = ccall((:my_alloc, "libmyalloc.so"), Int64, (Int64, ), n)
+    ret = ccall(all_ptr, Int64, (Int64, ), n)
     if (ret == -2) println("allocation table is full") end
     if (ret == -1) println("calloc could not allocate another bloc") end
     return ret
 end
 
 function free(num::Int)
-    ccall((:my_free, "libmyalloc.so"), Int64, (Int64, ), num)
+    ccall(fre_ptr, Int64, (Int64, ), num)
 end
 
 function set_values(num::Int, vals::Array{Float64, 1})
-    ret = ccall((:set_values, "libmyalloc.so"), Int64, (Int64, Int64, Ptr{Float64}), size(vals, 1), num, vals)
+    ret = ccall(set_ptr, Int64, (Int64, Int64, Ptr{Float64}), size(vals, 1), num, vals)
     return ret
 end
 
 function get_values(num::Int, vals::Array{Float64, 1})
-    ret = ccall((:get_values, "libmyalloc.so"), Int64, (Int64, Int64, Ptr{Float64}), size(vals, 1), num, vals)
+    ret = ccall(get_ptr, Int64, (Int64, Int64, Ptr{Float64}), size(vals, 1), num, vals)
     return ret
 end
 
