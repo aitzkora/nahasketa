8db76a3fae536acae37317bafc1f681e69ad0b75
aitzkora <marc.fuentes@tiscali.it>
2019-10-24 18:35:53 +0200

    we managed to use the BFGS of Optim with a functional coded in MPI \o/

--- julia/interfaces/fortran/withMPI/par_error.f90
+++ julia/interfaces/fortran/withMPI/par_error.f90
@@ -2,7 +2,6 @@ module par_error
     use mpi
     use iso_c_binding, only: c_int32_t, c_double
     public:: compute_error
-    private:: compute_slice, cumsum
 contains
 
     ! the calling subroutine
@@ -18,70 +17,15 @@ contains
         real(c_double), intent(out) :: df(n)
 
         real(c_double) ::  f_loc
-        real(c_double), allocatable:: df_loc(:)
-        integer(c_int32_t) :: slice(2)
-        integer(c_int32_t) :: code, nb_procs, rank, is, ie, loc_size
-        integer(c_int32_t), allocatable :: sizes(:), displacements(:)
+        integer(c_int32_t) :: code
         
         f = 0.d0
-        call MPI_COMM_SIZE( MPI_COMM_WORLD, nb_procs, code)
-        call MPI_COMM_RANK( MPI_COMM_WORLD, rank, code)
-       
-        !! computing slices
-        slice = compute_slice(n, nb_procs, rank)
-        is = slice(1)
-        ie = slice(2)
         ! computing objective
-        f_loc = 0.5d0 * sum( (x(is:ie) - c(is:ie))**2 )
-        call MPI_REDUCE( f_loc, f, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD, code )
+        f_loc = 0.5d0 * sum( (x - c)**2 )
+        call MPI_ALLREDUCE( f_loc, f, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD, code )
         
         ! computing gradient and ...
-        df_loc = x(is:ie) - c(is:ie)
- 
-        ! communicate it
-
-        loc_size = ie - is + 1
-        if (rank == 0) allocate( sizes(nb_procs) )
-        call MPI_GATHER( loc_size, 1, MPI_INT, sizes, 1, MPI_INT, 0, MPI_COMM_WORLD, code)
-        
-        if (rank == 0) displacements = cumsum(sizes)
-        call MPI_GATHERV( df_loc , loc_size, MPI_DOUBLE, &
-                          df, sizes, displacements, MPI_DOUBLE, 0, MPI_COMM_WORLD, code)
- 
-        
+        df = x - c
     end subroutine compute_error
-
-    ! private helper function
-    pure function compute_slice(n, procs, rank) result(s)
-        integer, intent(in) :: n, procs, rank
-        integer, dimension(2) :: s
-        integer :: length 
-        length =  n / procs
-        if ( mod(n, procs) == 0 ) then
-            s(1) = rank * length + 1
-            s(2) = s(1) + length - 1
-        else
-            if (rank < procs - 1) then
-                s(1) = rank * (length + 1) + 1
-                s(2) = s(1) + (length + 1) - 1
-            else
-                s(1) = rank * (length + 1) + 1
-                s(2) = s(1) + mod(n, length + 1) - 1
-            end if
-        end if
-    end function compute_slice
-
-    pure function cumsum(x) result(y)
-        implicit none
-        integer(c_int32_t), intent(in) :: x(:)
-        integer(c_int32_t) :: y(size(x,1))
-        integer :: i
-        y(1) = 0
-        do i =2, size(x, 1)
-           y(i) = y(i-1) + x(i-1)
-        end do
-
-    end function cumsum
-
 
 end module par_error
--- julia/interfaces/fortran/withMPI/test_optim.jl
+++ julia/interfaces/fortran/withMPI/test_optim.jl
@@ -1,5 +1,6 @@
 import MPI
-
+using Optim
+using LinearAlgebra
 function usage(rank)
   if (rank == 0) 
       println("Usage: mpirun -np nb_procs julia test_optim.jl N")
@@ -8,34 +9,73 @@ function usage(rank)
   exit()
 end
 
+function simu(x::Array{Float64,1})
+    f = Ref{Float64}(0.)
+    c = cos.(1:n)[slice[1]:slice[2]]
+    df = zeros(slice[2]-slice[1]+1)
+    @assert  size(c,1) == size(x, 1)
+    ccall((:compute_error, "./libpar_error.so"), 
+          Cvoid, (Ref{Int32}, Ptr{Float64}, Ptr{Float64}, Ref{Float64}, Ptr{Float64}), size(x, 1), x, c, f, df)
+    return f[], df
+end
+
+function cost(x::Array{Float64,1})
+    f,G = simu(x)
+    return f
+end
+
+function grad(x::Array{Float64,1})
+    f,G = simu(x)
+    return G
+end 
+
 function main()
     MPI.Init()
     comm = MPI.COMM_WORLD
-    comm_size = MPI.Comm_size(comm)
+    nb_procs = MPI.Comm_size(comm)
     rank = MPI.Comm_rank(comm)
     n_arg = size(ARGS, 1)
     if (n_arg < 1 )
         usage(rank)
     end
+    global n, slice
     n = parse(Int32, ARGS[1])
-
     x = zeros(n)
+    slice = compute_slice(n, Int32(nb_procs), Int32(rank))
     x[:] = 1:n
-    f,df = simu(x)
+    x_loc = x[slice[1]:slice[2]]
+    f,df = simu(x_loc)
     if (rank == 0) 
-        println("cost function and gradient" , f, df)
+        println("cost function at x0 = " , f)
+    end
+ 
+    res = optimize(cost, grad, x_loc, BFGS(); inplace =false)
+    x_min = Optim.minimizer(res)
+    #println("res = ", x_min)
+    x_glob = MPI.Gatherv(x_min, Cint[5,5], 0, comm)
+    if (rank == 0)
+        println(" |sol - cos(1:$n)| = ", norm(x_glob-cos.(1:n)))
     end
     MPI.Finalize()
 end
 
-function simu(x)
-    f = Ref{Float64}(0.)
-    n = convert(Int32, size(x, 1))
-    c = cos.(1:n)
-    df = zeros(n)
-    ccall((:compute_error, "./libpar_error.so"), 
-          Cvoid, (Ref{Int32}, Ptr{Float64}, Ptr{Float64}, Ref{Float64}, Ptr{Float64}), n, x, c, f, df)
-    return f[] ,df
-end
+
+function compute_slice(n::Int32, procs::Int32, rank::Int32) 
+    s = zeros(Int32, 2)
+    length =  n / procs
+    if ( mod(n, procs) == 0 ) 
+        s[1] = rank * length + 1
+        s[2] = s[1] + length - 1
+    else
+        if (rank < procs - 1)
+            s[1] = rank * (length + 1) + 1
+            s[2] = s[1] + (length + 1) - 1
+        else
+            s[1] = rank * (length + 1) + 1
+            s[2] = s[1] + mod(n, length + 1) - 1
+        end 
+    end
+    return s
+end 
 
 main()
