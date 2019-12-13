93a2c1c7038a62112ddd7e10baa503baa28e1e16
aitzkora <marc.fuentes@tiscali.it>
2019-12-13 16:17:50 +0100

    add a example of computing an adjoint in julia : it works

--- julia/adjoint/linear_example.jl
+++ julia/adjoint/linear_example.jl
@@ -23,12 +23,21 @@ using SparseArrays
 function compute_grad_by_adjoint(p)
    n  = size(p, 1)
    xₙ = direct(0.1, n, [1.; 0], p)
-   λ = [0 , 2* xₙ[2]]
+   λ = [0 , 2. * xₙ[2]]
    ∇g = zeros(1,n) 
    for i=n:-1:1
-     λ = A(0.1)  * λ
      ∂ₚf = sparse([2], [i], 1., 2, n)
      ∇g += λ' * ∂ₚf 
+     λ = A(0.1)  * λ
    end             
    return ∇g
 end
+
+function compute_diff(p, ε = 1.e-7)
+    n  = size(p,1)
+    ∇g = zeros(n)
+    for i=1:n
+        ∇g[i] = ε^(-1).*(cost(p + [ε * (i == j) for j=1:n])-cost(p))
+    end
+    return ∇g
+end 
