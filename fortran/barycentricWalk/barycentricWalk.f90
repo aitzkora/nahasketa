db16ff0bfb0e69a46cac3660dcd170c9974596e9
aitzkora <marc.fuentes@inria.fr>
2023-04-03 22:04:24 +0200

    barycentricWalk now works for triangle! tested on a generic case

--- fortran/barycentricWalk/barycentricWalk.f90
+++ fortran/barycentricWalk/barycentricWalk.f90
@@ -1,8 +1,9 @@
 module find_cell
+  use iso_fortran_env
   implicit none
   integer, parameter :: fp = kind(1.d0)
   private
-  public:: find_triangle_classic, fp
+  public:: find_triangle_classic, fp, find_triangle_walk
 
 contains
 
@@ -43,20 +44,35 @@ contains
     integer, intent(in) :: nb_cell
     logical                 ,allocatable :: flag_in (:)
     real   (kind=8)         ,allocatable :: xtri    (:,:)
+    real   (kind=8)         :: xmin, xmax, zmin,zmax
     integer :: i_cell, nb_edge, k, node
     nb_edge = 3
     cell = 0
-    allocate(flag_in(nb_edge))
-    allocate(xtri(2, nb_edge))
-    !xtri = spread([0.d0, 0.d0], 1 , nb_edge)
+    xmin=dble(minval(x_node(1,:)))
+    xmax=dble(maxval(x_node(1,:)))
+    zmin=dble(minval(x_node(2,:)))
+    zmax=dble(maxval(x_node(2,:)))
+    !! if not we leave 
+    if(coo(1) < xmin .or. coo(1) > xmax .or. &
+       coo(2) < zmin .or. coo(2) > zmax) then
+       print *, "hors bornes"
+       return
+    end if
+    !! otherwize we look for the cell.
+
+!! loop over all cell using OMP 
+    !$OMP PARALLEL DEFAULT(shared) PRIVATE(i_cell,k,node,xtri,flag_in)
+    allocate(flag_in   (nb_edge))
+    allocate(xtri    (2,nb_edge))
+    !$OMP DO
     do i_cell=1,nb_cell  
-      !if(cell .ne. 0) cycle !! for omp
+      if(cell .ne. 0) cycle !! for omp
       !! get coordinates of the nodes of the cell
       do k=1,nb_edge
         node      =  cell_node(k,i_cell)
         xtri(:,k) = dble(x_node(:,node))
       end do
-
+ 
       !! It is in the cell if coo_input and node(k) are on the same side
       !! of the line node(i)--node(j) for all k where i != j != k
       !! ---------------------------------------------------------------
@@ -66,13 +82,12 @@ contains
       call on_side_2d(coo,xtri(:,3),xtri(:,1),xtri(:,2),flag_in(3))
       if(all(flag_in)) then
         cell = i_cell
-        exit
       end if
     end do
+    !$OMP END DO
     deallocate(flag_in)
     deallocate(xtri)
-
-    return
+    !$OMP END PARALLEL
 
   end subroutine find_triangle_classic
 
@@ -81,28 +96,32 @@ contains
     real(fp), intent(in) :: coo(2)
     real(fp), intent(in) :: xtri(2,3)
     real(fp), intent(out) :: theta(3)
-    real(fp) :: denom, a, b, c, d
+    real(fp) :: denom, a, b, c, d, rhs(2)
 
-    a = xtri(1,2)-xtri(1,3) 
-    b = xtri(2,3)-xtri(2,2) 
-    denom = a * (xtri(1,1)-xtri(1,3)) + b * (xtri(2,1)-xtri(2,3))
+    a = xtri(1,1) - xtri(1,3) 
+    b = xtri(1,2) - xtri(1,3) 
+    c = xtri(2,1) - xtri(2,3) 
+    d = xtri(2,2) - xtri(2,3) 
 
-    if (abs(denom) < 1e-10) then
-       print *, "pb in compute_barycenters"
+    denom = a * d - b * c
+
+    if (abs(denom) < 1e-10 * min(norm2([a,c]),norm2([b,d]))) then
+       print *, "compute_barycenters : find a small triangle, use order one approx"
        stop -1
     endif
 
-    c = coo(1) - xtri(1, 3)
-    d = coo(2) - xtri(2, 3)
+    rhs = coo(1) - xtri(:, 3)
 
-    theta(1) = (a * c + b *d) / denom
-    theta(2) = ((xtri(2,3) - xtri(2,1)) * c + &
-                (xtri(1,1) - xtri(1,3)) * d) / denom
+    theta(1) = (rhs(1) * d - rhs(2) * b) / denom
+    theta(2) = (rhs(2) * a - rhs(1) * c) /denom
     theta(3) = 1.d0 - theta(1) - theta(2)
 
   end subroutine compute_barycenters
 
-
+  !> subroutine to locate a triangle in a triangular mesh using coordinates
+  !> taken from article "Efficient implementation of characteristic-based schemes 
+  !> on unstructured triangular grids" by S. Cacace and R. Ferretti
+  !> @note : compared to the brute force algorithm we need the neigbor array
   subroutine find_triangle_walk(coo, cell, x_node, cell_node, neighbors, nb_cell, cell_0)
     real(fp), intent(in) :: coo(2)
     integer, intent(out) :: cell
@@ -127,8 +146,13 @@ contains
       if (all(theta > 0)) then
         find = .true.
       else
-        chg = maxloc(abs(theta), 1, theta <= 0)  
-        
+        chg = maxloc(abs(theta), 1, theta <= 0 .and. neighbors(:, cell) /= -1)
+        if (chg /= 0) then
+           cell = neighbors(chg, cell)
+        else    
+           cell = 0 
+           return
+        end if
       end if
     end do
 
@@ -139,11 +163,11 @@ end  module find_cell
 program test_find_cell
     use find_cell
     implicit none
-    integer :: sx, cell, nb_cells, nb_nodes, nb_neighbors
+    integer :: sx, nb_cells, nb_nodes, nb_neighbors, nb_finds, i
     real(fp), allocatable :: x_node(:,:)
-    integer, allocatable:: cell_node(:,:), neighbors(:,:)
-    real(fp), allocatable :: coo(:)
-    
+    integer, allocatable:: cell_node(:,:), neighbors(:,:), res_clas(:), res_walk(:)
+    real(fp), allocatable ::  x_min(:), x_max(:), coo(:,:), alea(:,:)
+    real :: start_time, stop_time
     ! read nodes
     open(22, file='x_node.txt')
     read(22,*) sx, nb_nodes
@@ -168,12 +192,44 @@ program test_find_cell
     close(22)
     print "(a,i0)", "nb_neighbors = ", nb_neighbors
 
-    coo = [0.d0, 0.d0]
-    call find_triangle_classic(coo, cell, x_node, cell_node, nb_cells)
-    print '(a,2(f6.3,1x),a,i0)', 'cell containing (', coo, ') -> ', cell
-    
-    call find_triangle_walk(coo, cell, x_node, cell_node, neighbors, nb_cells)
-    print '(a,2(f6.3,1x),a,i0)', 'cell containing (', coo, ') -> ', cell
+    x_min = [0.,0.]
+    x_max = x_min
+    x_min = minval(x_node, dim=2)
+    x_max = maxval(x_node, dim=2)
+
+    nb_finds = 30
+    alea = reshape(spread(0.d0, 1, nb_finds*2), [2, nb_finds])
+    call random_number(alea)
+    !coo(1,:) = coo(1,:) * (x_max(1)-x_min(1)) + x_min(1)
+    !coo(2,:) = coo(2,:) * (x_max(2)-x_min(2)) + x_min(2)
+    allocate(coo(2, nb_finds))
+
+    coo(1,:) = alea(2,:) * cos(4.d0*atan(1.d0) * alea(1,:))
+    coo(2,:) = alea(2,:) * sin(4.d0*atan(1.d0) * alea(1,:))
+
+    call cpu_time(start_time)
+        
+    res_clas = spread(0, 1, nb_finds)
+    do i=1,nb_finds
+      call find_triangle_classic(coo(:,i), res_clas(i), x_node, cell_node, nb_cells)
+    end do
+    call cpu_time(stop_time)
+    print *, "tps cpu classique", stop_time-start_time
+
+    call cpu_time(start_time)
+    res_walk = spread(0, 1, nb_finds)
+    do i=1,nb_finds
+      call find_triangle_walk(coo(:,i), res_walk(i), x_node, cell_node, neighbors, nb_cells)
+    end do
+    call cpu_time(stop_time)
+    print *, "tps cpu barycentric", stop_time-start_time
+   
+    CHECK: if (all(res_walk == res_clas)) then
+      print *, "**************** SUCCESS *************************"
+    else
+      print *, "XXXXXXXXXXXXXXXX ERROR XXXXXXXXXXXXXXXXXXXXXXXXXXX"
+      print *, sum(pack(spread(1,1,nb_finds), (res_walk /= res_clas))), " distinct results" 
+    end if CHECK
 
 end program test_find_cell
 
---
+++ fortran/barycentricWalk/calc_bary.jl
@@ -0,0 +1,37 @@
+using LinearAlgebra
+function calc_bary(x, x0)
+
+  a = x[1,1] - x[1,3] 
+  b = x[1,2] - x[1,3]
+  c = x[2,1] - x[2,3]
+  d = x[2,2] - x[2,3]
+  denom = a * d - b * c
+
+  if (abs(denom)/min(norm([a;c]),norm([b;d])) < 1e-10)
+     # les 2 vecteurs sont colinéaires
+     error("too much dependant vectors")
+  end
+  rhs = x0 - x[:, 3]
+  theta = zeros(3)
+  theta[1] = (rhs[1] * d - rhs[2] * b) / denom
+  theta[2] = (rhs[2] * a - rhs[1] * c) / denom
+  theta[3] = 1.0 - theta[1] - theta[2]
+  return theta
+end
+
+function bary2(x, x0)
+ theta = (x[:,1:2]-(x[:,[3,3]]))\(x0-x[:,3])
+ theta = [theta; 1.0 - sum(theta)]
+end
+
+function patho_x(epsi = 1e-13)
+   x = rand(2,2)
+   v3 = epsi*(x[:,2]-x[:,1])
+   x = [ x[:,1] v3+x[:,1] v3 ]
+end
+
+
+
+
+
+
