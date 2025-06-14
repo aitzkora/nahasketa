b01f2f50d600fd7e6188e8dc56a633761acecf72
aitzkora <marc.fuentes@inria.fr>
2025-06-14 09:47:51 +0200

    killmetis : metis_to_mesh seems to be ✔

--- fortran/KillMetis/dual_and_part.F90
+++ fortran/KillMetis/dual_and_part.F90
@@ -20,9 +20,6 @@ program skotx_direkt
   integer(is), parameter ::   nn           = 7
   integer(is) ::              eptr(7)      = [ 0, 3, 6, 9, 12, 15, 18 ]
   integer(is) ::              eind(18)     = [ 0, 1, 2, 0, 1, 5, 1, 5, 4, 1, 4, 6, 1, 6, 3, 1, 3, 2 ]
-  integer(is) ::              xadj_c(7)    = [ 0, 5, 10, 15, 20, 25, 30 ]
-  integer(is) ::              adjncy_c(30) = [ 1, 2, 3, 4, 5, 0, 2, 3, 4, 5, 0, 1, 3, 4, 5, 0, 1, 2, 4, 5, 0, &
-                                               1, 2, 3, 5, 0, 1, 2, 3, 4 ]
 
   type :: mesh_scotch 
      integer(is)              :: velmbas
@@ -30,18 +27,11 @@ program skotx_direkt
      integer(is)              :: velmnbr
      integer(is)              :: vnodnbr
      integer(is), allocatable :: verttab(:)
-     integer(is), allocatable :: vendtab(:)
-     integer(is), allocatable :: velotab(:)
-     integer(is), allocatable :: vnlotab(:)
-     integer(is), allocatable :: vlbltab(:)
      integer(is)              :: edgenbr
      integer(is), allocatable :: edgetab(:)
   end type mesh_scotch
-
   
   type(mesh_scotch) :: mesh
-
-
 
   allocate (epart(ne))
   allocate (npart(nn))
@@ -61,19 +51,18 @@ contains
      integer(is), intent(in) :: edgetab(:)
 
      integer(is) :: vertnum, degrmax, edgeadj
-     integer(is), allocatable :: srcverttax(:)
-     integer(is) :: edgechk 
+     integer(is), allocatable :: srcverttax(:), srcedgetax(:), tmp(:)
+     integer(is) :: edgechk, edgenum, velmnnd
 
+     velmnnd = velmnbr + baseval
 
-
-     allocate(srcverttax(baseval:vnodnbr+velmnbr+baseval-1))
+     allocate(srcverttax(baseval:vnodnbr+velmnbr+baseval))
 
      edgechk = verttab(velmnbr+baseval)
-     print '(a,i0)', "edgecheck = ", edgechk
 #ifndef NDEBUG
-     if ((lbound(edgetab, 1) /= baseval) .or. (ubound(edgetab,1) /= (edgechk-1))) then
-        print "(a,i0,a,i0,a)", "edgetab(",lbound(edgetab, 1), ":", ubound(edgetab,1),")"
-        stop "bad bounds for edgtab"
+     if ((lbound(edgetab, 1) /= baseval) .or. & 
+         (ubound(edgetab,1) /= (edgechk-1))) then
+        stop "bad bounds for edgetab"
      endif
 #endif
      srcverttax(velmnbr:) = 0
@@ -83,7 +72,7 @@ contains
      ! loop on elems
      do vertnum= baseval, baseval+velmnbr-1
        block
-         integer(is) :: edgenum, edgennd, degrval, ind
+         integer(is) :: edgennd, degrval, ind
          edgenum = verttab(vertnum)
          edgennd = verttab(vertnum+1)
          degrval = edgennd - edgenum
@@ -104,46 +93,52 @@ contains
      if (edgenbr /= edgechk-baseval) stop "error in computation of edgenbr"
 #endif
      edgenbr = edgenbr * 2
-   
-     print '(a,i0)', "edgenbr = ", edgenbr
 
-     if (eptr(baseval) = baseval) then
-       srcverttax(baseval:baseval+vnodnbr-1) = eptr(baseval:baseval+vnodnbr-1)
+     if (verttab(baseval) == baseval) then
+       srcverttax(baseval:baseval+velmnbr-1) = verttab(baseval:baseval+velmnbr-1)
      else
-       edgeadj = eptr(baseval) - baseval
+       edgeadj = verttab(baseval) - baseval
        do vertnum = baseval, baseval + velmnbr -1
-         srcverttax(vertnum) = eptr(vertnum) + edgeadj
+         srcverttax(vertnum) = verttab(vertnum) + edgeadj
        end do
      end if
 
-     !edgenum = eptr(velmnbr + baseval)
-     !do vertnum = baseval + velmnbr, velmnbr + baseval + vnodnbr
-     !  block
-     !    integer(is):: degrval
-     !    degrval = srcverttax(vertnum)
-     !    if (degrval > degrmax) then degrmax = degrval
-     !    srcverttax(vertnum) = edgenum
-     !    edgenum = edgenum + degrval
-     !  end block
-     !end do
-     !srcverttax(vertnum) = edgenum
-     !allocate(srcedgetax(0:edgenbr-1))
+     edgenum = verttab(velmnnd)
+     do vertnum = velmnnd, velmnnd + vnodnbr - 1
+       block
+         integer(is) :: degrval
+         degrval = srcverttax(vertnum)
+         if (degrval > degrmax) degrmax = degrval
+         srcverttax(vertnum) = edgenum
+         edgenum = edgenum + degrval
+       end block
+     end do
+     srcverttax(vertnum) = edgenum
+     
+     allocate(srcedgetax(baseval:baseval+edgenbr-1))
 
-     !do edgenum = baseval, eptr(velmnbr+baseval)-1
-     !  srcedgetax(edgenum) = edgetax(edgenum) + ne
-     !end do
 
-     !do vertnum = baseval, ne+ baseval -1
-     !  do edgenum = eptr(vertnum), eptr(vertnum + 1)
-     !    srcedgetax(srcverttax(edgetax(edgenum) + velmnbr ) ) = vertnum
-     !    srcverttax(edgetax(edgenum) + velmnbr ) = 
-     !    srcverttax(edgetax(edgenum) + velmnbr ) + 1
-     !  end do
-     !end do
+     do edgenum = baseval, verttab(velmnnd)-1
+       srcedgetax(edgenum) = edgetab(edgenum) + velmnbr
+     end do
 
-     !srcverttax(velmnbr+baseval:ne+baseval+vnodnbr-2)
-     !srcverttax(velmnbr+baseval) = eptr(velmnbr+baseval)
+     do vertnum = baseval, velmnnd -1
+       do edgenum = verttab(vertnum), verttab(vertnum + 1)-1
+         srcedgetax(srcverttax(edgetab(edgenum) + velmnbr ) ) = vertnum
+         srcverttax(edgetab(edgenum) + velmnbr ) = &
+         srcverttax(edgetab(edgenum) + velmnbr ) + 1
+       end do
+     end do
 
+     tmp = srcverttax(velmnnd:velmnnd+vnodnbr-2)
+     srcverttax(velmnnd+1:velmnnd+1+vnodnbr-2) = tmp(:)
+     srcverttax(velmnnd) = verttab(velmnnd)
+
+     mesh%edgetab = srcedgetax
+     mesh%verttab = srcverttax
+     mesh%edgenbr = edgenbr
+     mesh%vnodnbr = vnodnbr
+     mesh%velmnbr = velmnbr
    end subroutine metis_to_mesh
 
 
