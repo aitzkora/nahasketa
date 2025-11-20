37fae992b46a934e63eb871b03ec1a3ee111a5d0
aitzkora <marc.fuentes@inria.fr>
2025-11-20 16:33:26 +0100

    [julia] test_prec : detect denormalized

--- julia/test_prec.jl
+++ julia/test_prec.jl
@@ -11,9 +11,32 @@ struct MyFloat{T, EMIN, EMAX}
     @assert T isa Integer
     @assert EMIN isa Integer
     @assert EMAX isa Integer
+    if (m[1]==0) 
+       @warn "m is denormalized mantissa"
+    end
     return new{T, EMIN, EMAX}(m, e, s)
   end
 end
+
+"""
+outer constructor taking Vector of Bool
+"""
+function MyFloat{T, EMIN, EMAX}(m::Vector{Bool}, e::Int64, s::Bool) where {T, EMIN, EMAX}
+    return MyFloat{T, EMIN, EMAX}(SVector{T,Bool}(m...), e, s)
+end
+
+function emin(::Type{<:MyFloat{T, EMIN,  EMAX}}) where {T, EMIN, EMAX}
+   return EMIN
+end
+
+function emax(::Type{MyFloat{T, EMIN,  EMAX}}) where {T, EMIN, EMAX}
+   return EMAX
+end
+
+function precision(::Type{<:MyFloat{T, EMIN,  EMAX}}) where {T, EMIN, EMAX}
+   return T
+end
+
 
 """
 add two SVector{T,Bool} with carry
@@ -23,15 +46,18 @@ function add_mantissa(x::SVector{T,Bool}, y::SVector{T,Bool}) where {T}
     for i=T:-1:2
       tmp = z[i] + x[i] + y[i]
       if (tmp >= 2)
-          z[i-1] = 1
-          z[i] = tmp - 2
+         #if (i == 1) 
+         #    throw(OverflowError("mantissa is to small"))
+         #end
+         z[i-1] = 1
+         z[i] = tmp - 2
       else
           z[i] = tmp
       end
     end
-    tmp = x[1] + y[1] + z[1]
+    tmp = x[1] + y[1]
     if (tmp>= 2)
-        throw(OverflowError("mantissa is to small"))
+        @error "overflow"
     else
         z[1] = tmp
     end
@@ -47,77 +73,77 @@ using Test
  s2 = SVector{3, Bool}(1, 0, 1)
  s3 = SVector{3, Bool}(1, 1, 1)
 
- @test add_mantissa(s1, s2) == s3
-
- @test_throws OverflowError add_mantissa(s1, s3)
-
-end
-
-using LinearAlgebra
-
-function eoshift(x::SVector{T,Bool}, shf::Int64 = 0) where T
-  if (shf >= 0)
-      return vcat(SVector{T-shf,Bool}(x[shf+1:end]), SVector{shf, Bool}(zeros(shf)))
-  else
-      throw(DomainError("shf must be positive"))
-  end
+ #@test add_mantissa(s1, s2) == s3
+ #@test_throws OverflowError add_mantissa(s1, s3)
 end
 
 function Base.:<<(x::SVector{T,Bool}, shf::Int64 = 0) where T
-  return eoshift(x, shf)
+  if (shf >= 0) && (shf <= T)
+      return vcat(SVector{T-shf,Bool}(x[shf+1:end]), SVector{shf, Bool}(zeros(shf)))
+  else
+      throw(DomainError("shf = $shf ̸∈ [0,$T]"))
+  end
 end
 
-function plus(x::MyFloat{T,EMIN, EMAX}, y:: MyFloat{T,EMIN, EMAX}) where {T, EMIN, EMAX}
+# addition
+function Base.:+(x::MyFloat{T,EMIN, EMAX}, y:: MyFloat{T,EMIN, EMAX}) where {T, EMIN, EMAX}
     @assert (x.s == false) && (x.s == y.s)
     if (y.e > x.e)
-      return plus(y, x)
+      return Base.:+(y, x)
     end
     shf = x.e-y.e
     expo = x.e
-    if (shf > T + 2)
+    if (shf > T)
         return MyFloat{T, EMIN, EMAX}(x.m, expo, x.s) 
     end
     m_add = add_mantissa( x.m, y.m << shf)
     return MyFloat{T, EMIN, EMAX}(m_add, expo, x.s)
 end    
 
-function MyFloat{T, EMIN, EMAX}(x::Float64) where {T,EMIN, EMAX}
+# cast Julia's float to MyFloat
+function MyFloat{T, EMIN, EMAX}(x::F) where {T,EMIN, EMAX, F <: AbstractFloat}
     @assert x >= 0
     e = Int64(floor(log2(x))) + 1
-    k = Int(floor(abs(x)/exp2(e-t)))
-    m = Bool[digits(k; base=2, pad=t)...]
+    k = Int(ceil(abs(x)/exp2(e-T)))
+    m = Bool[digits(k; base=2, pad=T)[end:-1:1]...]
     m = m[end:-1:1]
-    MyFloat{T, EMIN, EMAX}(m, e, false)
+    MyFloat{T, EMIN, EMAX}(SVector{T,Bool}(m), e, false)
 end
 
-function str_to_exp(x::Int64) 
+# print functions
+
+function exp_string(x::Int64) 
     exps = ["⁰", "¹", "²", "³", "⁴", "⁵", "⁶", "⁷", "⁸", "⁹"]
     dic = Dict([string.([0:9;]) .=>  exps; "-" =>"⁻"])
-    reduce(*,(x->dic[x]).([string(c) for c in string(x)]))
+    reduce(*, (x->dic[x]).([string(c) for c in string(x)]))
 end
 
 function Base.show(io::IO, z::MyFloat{T, EMIN, EMAX}) where {T, EMIN, EMAX}
   str_2 = "("*reduce(*,map(x->string((x ? 1 : 0)),z.m))*")₂"
-  str_2 *= "×2"*str_to_exp(z.e-T)
+  str_2 *= "×2"*exp_string(z.e-T)
   print(io, "[" * str_2 * ", " * string(exp2.([T-1:-1:0;])'*z.m*exp2(z.e-T)) * "]")
 end
 
-
-function Base.:+(x::MyFloat{T, EMIN, EMAX}, y::MyFloat{T, EMIN, EMAX}) where {T,EMIN, EMAX}
-    return plus(x, y)
-end
-
-
+# Some constants
 """
-the Nick Higham's Book small float type 
-t = 3
-emin = -1
-emax = 3
+the Nick Higham's Book small float type
 """
 const FloatNH = MyFloat{3, -1, 3}
 
+z1 = FloatNH(Bool[1,0,1], -1, false)
+z2 = FloatNH(Bool[1,0,0], -2, false)
+#z3 = FloatNH(2.0)
 
-z2 = MyFloat{3, -1, 3}(Bool[0,1,0], 3, false)
-z1 = MyFloat{3, -1, 3}(Bool[1,0,0], 2, false)
+println(z1 , " + ", z1, " -> ", z1 + z1 )
 
-println(z1 , " + ", z2, " -> ", z1 + z2 )
+const MyFloat16 = MyFloat{11, -14, 15}
+
+function enumerate(::Type{<:MyFloat{T, EMIN, EMAX}}) where {T, EMIN, EMAX}
+    estimation = (1<<T) * (EMAX-EMIN)
+    if (estimation > 100)
+        return   estimation
+    else
+       return [ MyFloat{T,EMIN,EMAX}(Bool[digits(m; base=2, pad=T)[end:-1:1]...],e, false) for e=EMIN:EMAX for m=1<<(T-1):(1<<T-1)]
+    end
+end 
+
